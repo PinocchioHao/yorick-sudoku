ad2178112cb615fdd52c7a713b09a7d603462db1
PinocchioHao <435374771@qq.com>
2026-06-08 00:47:56 +1000

    feat: import vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// 引入 Vercel 的数据统计组件
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+        {/* 把统计组件挂载在 App 旁边，它会自动静默收集页面访问数据 */}
+        <Analytics />
+    </React.StrictMode>,
 )
